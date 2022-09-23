0b680d55c52842e5d3a22e742e574ee3ac7f1560
Austin Merrick <austinmerrick98@gmail.com>
2022-09-22 21:04:42 -0700

    Add progress indicator

---
+++ src/lib/ProgressIndicator.svelte
@@ -0,0 +1,33 @@
+<script>
+  // done, current, todo
+  export let state = "current" 
+</script>
+
+<div class="{state}">
+</div>
+
+<style>
+  div {
+    align-self: center;
+    box-sizing: border-box;
+    width: 1rem;
+    height: 1rem;
+    display: inline-block;
+    margin: .2rem;
+    border-radius: 100%;
+    transition: all .4s ease-out;
+    border: solid black 2px;
+  }
+
+  .current {
+    background-color: black;
+  }
+
+  .done {
+    background-color: gray;
+  }
+
+  .todo {
+    background-color: white;
+  }
+</style>
